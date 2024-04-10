dcaf4730dd1a51353930558869f87d050ac2e314
Aman Agnihotri <aman.agnihotri@yahoo.com>
2024-04-10 22:49:37 +0530

    Update main to manage a web server with mongodb

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,9 +4,94 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"ttt/lib"
+	"ttt/pkg/infra/data/db"
+	"ttt/pkg/infra/web/http"
+)
+
+const (
+	name    = "ttt"
+	address = ":8080"
+	maxLoad = 20_000
 )
 
 func main() {
-	log.Println("ok")
+	const timeout = 3 * time.Second
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	dbContext, err := lib.NewDBContext(ctx)
+	{
+		if err != nil {
+			log.Println(err)
+
+			return
+		}
+
+		if err = dbContext.Ping(ctx); err != nil {
+			log.Println(err)
+
+			return
+		}
+
+		log.Println("connected with mongodb;")
+	}
+
+	jwtContext, err := lib.NewJWTContext()
+	if err != nil {
+		log.Println(err)
+
+		return
+	}
+
+	mux := http.NewMux(jwtContext)
+
+	mux.Handle("/", func(ctx *http.Context) {
+		ctx.PlainText("ok")
+	})
+
+	server := http.NewServer(name, address, maxLoad, mux)
+
+	if err = server.Start(); err != nil {
+		log.Println(err)
+
+		return
+	}
+
+	log.Printf("listening on %s\n", address)
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+	<-done
+
+	shutdown(server, dbContext)
+}
+
+func shutdown(server *http.Server, dbClient db.Client) {
+	const timeout = 5 * time.Second
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	log.Println("shutdown initiated;")
+
+	if err := server.Stop(ctx); err != nil {
+		log.Println(err)
+	} else {
+		log.Println("server shutdown gracefully;")
+	}
+
+	if err := dbClient.Disconnect(ctx); err != nil {
+		log.Println(err)
+	} else {
+		log.Println("mongodb disconnected gracefully;")
+	}
 }
