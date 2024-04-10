7325b57218322c9bfb1856dd8560c28e008b1c9a
Aman Agnihotri <aman.agnihotri@yahoo.com>
2024-04-10 23:54:23 +0530

    Add winner method for game

---
+++ internal/base/domain/game/winner/method.go
@@ -0,0 +1,12 @@
+// Copyright 2024 Aman Agnihotri
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package winner
+
+type Method string
+
+const (
+	Play          Method = "play"
+	Timeout       Method = "timeout"
+	Disconnection Method = "disconnection"
+)
