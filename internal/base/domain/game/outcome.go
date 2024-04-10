be3afbc5cca927fc097c231a7453b553f8a7c0d4
Aman Agnihotri <aman.agnihotri@yahoo.com>
2024-04-10 23:53:47 +0530

    Add outcome for game

---
+++ internal/base/domain/game/outcome.go
@@ -0,0 +1,12 @@
+// Copyright 2024 Aman Agnihotri
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package game
+
+type Outcome string
+
+const (
+	Win         Outcome = "win"
+	Draw        Outcome = "draw"
+	Interrupted Outcome = "interrupted"
+)
