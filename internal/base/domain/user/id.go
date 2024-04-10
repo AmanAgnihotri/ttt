fbfcf870c0007f94369ee21a5f45725335b8dca1
Aman Agnihotri <aman.agnihotri@yahoo.com>
2024-04-10 23:10:26 +0530

    Add id for user

---
+++ internal/base/domain/user/id.go
@@ -0,0 +1,26 @@
+// Copyright 2024 Aman Agnihotri
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package user
+
+import (
+	"ttt/pkg/domain/id"
+)
+
+type ID int64
+
+func NewID() (ID, bool) {
+	if i, ok := id.NewID(); ok {
+		return ID(i), true
+	}
+
+	return 0, false
+}
+
+func ParseID(i int64) (ID, bool) {
+	if id.IsValid(i) {
+		return ID(i), true
+	}
+
+	return 0, false
+}
