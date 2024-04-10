c42827e4d1e1c3f400dffb65e40f8947d528197d
Aman Agnihotri <aman.agnihotri@yahoo.com>
2024-04-11 03:59:20 +0530

    Add function to configure game module

---
+++ internal/game/module.go
@@ -0,0 +1,18 @@
+// Copyright 2024 Aman Agnihotri
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package game
+
+import (
+	"ttt/internal/base"
+	"ttt/internal/game/common"
+	"ttt/internal/game/core"
+	"ttt/internal/game/user"
+)
+
+func Configure(base base.Wire) {
+	wire := common.NewWire(base)
+
+	core.Configure(wire)
+	user.Configure(wire)
+}
