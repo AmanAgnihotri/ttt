468901652460d55b75d5155f318e6b51c8a7a2b0
Aman Agnihotri <aman.agnihotri@yahoo.com>
2024-04-14 08:43:33 +0530

    Add game and user stores to base wire

--- internal/base/wire.go
+++ internal/base/wire.go
@@ -5,24 +5,34 @@ package base
 
 import (
 	"ttt/internal/base/infra/data/app"
+	"ttt/internal/base/infra/data/game"
+	"ttt/internal/base/infra/data/user"
 	"ttt/pkg"
 )
 
 type Wire interface {
 	pkg.Wire
 	AppStore() *app.Store
+	GameStore() *game.Store
+	UserStore() *user.Store
 }
 
 func NewWire(appID string, pkg pkg.Wire) Wire {
 	return &wire{
 		pkg,
 		app.NewStore(appID, pkg.DBService()),
+		game.NewStore(pkg.DBService()),
+		user.NewStore(pkg.DBService()),
 	}
 }
 
 type wire struct {
 	pkg.Wire
-	appStore *app.Store
+	appStore  *app.Store
+	gameStore *game.Store
+	userStore *user.Store
 }
 
-func (w *wire) AppStore() *app.Store { return w.appStore }
+func (w *wire) AppStore() *app.Store   { return w.appStore }
+func (w *wire) GameStore() *game.Store { return w.gameStore }
+func (w *wire) UserStore() *user.Store { return w.userStore }
